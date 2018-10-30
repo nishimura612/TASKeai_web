f7e996f88690a4558b038d221028b5df8e86f726
koaji <38277028+koaji@users.noreply.github.com>
2018-10-30 16:06:15 +0900

    Update rpi/src/blinklight.php
    コマンド部分を分割して，変数に置き換え．

--- rpi/src/blinklight.php
+++ rpi/src/blinklight.php
@@ -1,21 +1,34 @@
 <?php
-	if(system('ls /sys/class/gpio | grep gpio4') != 'gpio4'){
-		system('echo 4 > /sys/class/gpio/export');
+
+	$gpioPortNum = '4';
+	$gpioPort = 'gpio' . $gpioPortNum;
+
+	$gpioMode = 'out';
+	$gpioModeState = '/sys/class/gpio/gpio4/direction';
+	$gpioState = '/sys/class/gpio/gpio4/value';
+
+	// gpioPort の初期がされているかチェック
+	if(system('ls /sys/class/gpio | grep ' . $gpioPort) != $gpioPort){
+		system('echo '. $gpioPortNum .' > /sys/class/gpio/export'); // ないなら初期化
 	}
-	$gpioStatus = system('cat /sys/class/gpio/gpio4/direction');
-	if($gpioStatus != 'out'){
-		system('echo out > /sys/class/gpio/gpio4/direction');
+
+	// gpioPort の動作モードチェック
+	if(system('cat ' . $gpioModeState) != $gpioMode){
+		system('echo ' . $gpioMode . ' > ' . $gpioModeState);
 	}
+
+	// stateLightの値によって，gpioPortの出力を変える．
 	if($_POST['stateLight'] == 'ON'){
-		system('echo 1 > /sys/class/gpio/gpio4/value');
+		system('echo 1 > ' . $gpioState);
 	}else if($_POST['stateLight'] == 'OFF'){
-		system('echo 0 > /sys/class/gpio/gpio4/value');
+		system('echo 0 > ' . $gpioState);
 	}else if($_POST['stateLight'] == 'INVERTED'){
-		$nowStatus = system('cat /sys/class/gpio/gpio4/value');
+		// gpioPortの状態を取得し，逆の状態をセットする．
+		$nowStatus = system('cat ' . $gpioState);
 		if($nowStatus == 0){
-			system('echo 1 > /sys/class/gpio/gpio4/value');
+			system('echo 1 > ' . $gpioState);
 		}else{
-			system('echo 0 > /sys/class/gpio/gpio4/value');
+			system('echo 0 > ' . $gpioState);
 		}
 	}
 ?>
