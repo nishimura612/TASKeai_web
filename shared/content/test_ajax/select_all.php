a73a5d19506697c85bd139447f035e52b0195d19
hirata0517masato <32904561+hirata0517masato@users.noreply.github.com>
2018-11-10 02:00:43 +0900

    select(ajax)
    ipアドレス:8081/test_ajax/select.html
    
    見た目はほぼ変化なし
    ただし、ajaxを使用したことにより5秒ごとにデータの更新を自動で行う。
    また、画面の再読み込みは行わない。

---
+++ shared/content/test_ajax/select_all.php
@@ -0,0 +1,19 @@
+﻿<?php
+$pdo = new PDO ( 'mysql:host=localhost;dbname=taskeai;charset=utf8', 'root', 'vagrant' );
+$sql = $pdo->query ( 'select * from taskeai.user join taskeai.task using(uid)' );
+
+print('<table>');
+print('<tr><td>uid</td><td>uname</td><td>tid</td><td>tname</td><td>done</td><td>timelimit</td></tr>');
+foreach ( $sql->fetchAll () as $row ) {
+    print('<tr>');
+    print("<td> ${row['uid']} </td>");
+    print("<td> ${row['uname']} </td>");
+    print("<td> ${row['tid']} </td>");
+    print("<td> ${row['tname']} </td>");
+    print("<td> ${row['done']} </td>");
+    print("<td> ${row['timelimit']} </td>");
+    print('</tr>');
+}
+print('</table>');
+
+?>
